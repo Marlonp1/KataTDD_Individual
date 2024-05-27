c242e843648e8cbd738ea304f67071d9cc7327bf
MarlonP1 <60829809@continental.edu.pe>
2024-05-27 17:10:17 -0500

    Caso 3: Numeros negativos (Verde)

--- src/logica/ejercicio.py
+++ src/logica/ejercicio.py
@@ -4,4 +4,6 @@ class Ejercicio:
             raise ValueError("Las listas de números y pesos deben tener la misma longitud")
         total_weighted_sum = sum(num * weight for num, weight in zip(numeros, pesos))
         total_weight = sum(pesos)
+        if total_weight == 0:
+            return 0  # Si la suma de pesos es cero, devolver 0 para evitar la división por cero
         return total_weighted_sum / total_weight
