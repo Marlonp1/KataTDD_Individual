17605a47d302fd03a333f083e7aef8d3dd802f9b
MarlonP1 <60829809@continental.edu.pe>
2024-05-27 17:01:20 -0500

    Caso 2: Listas vacías o nulas (verde)

--- src/logica/ejercicio.py
+++ src/logica/ejercicio.py
@@ -1,11 +1,11 @@
 class Ejercicio:
     def weighted_average(self, numeros, pesos):
 
-        def weighted_average(self, numeros, pesos):
-            if len(numeros) == 0 or len(pesos) == 0:
-                return None  # Devolver None si una de las listas está vacía
-            if len(numeros) != len(pesos):
-                raise ValueError("Las listas de números y pesos deben tener la misma longitud")
-            total_weighted_sum = sum(num * weight for num, weight in zip(numeros, pesos))
-            total_weight = sum(pesos)
-            return total_weighted_sum / total_weight
+
+        if len(numeros) == 0 or len(pesos) == 0:
+            return 0  # Devolver 0 si una de las listas está vacía
+        if len(numeros) != len(pesos):
+            raise ValueError("Las listas de números y pesos deben tener la misma longitud")
+        total_weighted_sum = sum(num * weight for num, weight in zip(numeros, pesos))
+        total_weight = sum(pesos)
+        return total_weighted_sum / total_weight
