9742fff384f957190ccbd255a388f9220443127d
MarlonP1 <60829809@continental.edu.pe>
2024-05-27 16:57:46 -0500

    Caso 1: Longitud Diferente de Listas de Números y Pesos (Verde)

--- src/logica/ejercicio.py
+++ src/logica/ejercicio.py
@@ -1,7 +1,7 @@
 class Ejercicio:
     def weighted_average(self, numeros, pesos):
         if len(numeros) != len(pesos):
-            return -1
+            raise ValueError("Las listas de números y pesos deben tener la misma longitud")
         total_weighted_sum = sum(num * weight for num, weight in zip(numeros, pesos))
         total_weight = sum(pesos)
         return total_weighted_sum / total_weight
